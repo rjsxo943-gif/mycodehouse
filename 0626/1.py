9b3be7b6519416283fec86195d714431fe993cce
rjsxo943-gif <229681393+rjsxo943-gif@users.noreply.github.com>
2026-06-26 12:40:08 +0900

    Add basic calculator and list examples

---
+++ 0626/1.py
@@ -0,0 +1,13 @@
+y = []
+
+i = 0
+c=0
+for  i in range(5):    
+    a = int(input("5개의 정수값을 입력하시오: "))
+    y.append(a)
+
+
+c = int(len(y))
+
+print(y)
+print(f'mean = {sum(y)/c}, sum = {sum(y)}, min = {min(y)}, max = {max(y)}')
---
+++ 0626/2.py
@@ -0,0 +1,9 @@
+fruits = []
+
+for i in range(5):
+    fruit = input("enter a fruit: ")
+    fruits.append(fruit)
+
+print(fruits)
+print(fruits[-1])
+print(fruits[3])
