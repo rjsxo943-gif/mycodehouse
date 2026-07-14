65d5616f09f4309e7fa462fc0b742d83b515a8c5
busanseojung22 <busanseojung22@gmail.com>
2026-07-14 14:35:55 +0900

    Add calculator feature

--- 0622/python.py
+++ 0622/python.py
@@ -1,3 +1,8 @@
 print("Welcome to Python")
-print("1 + 2 =", 1+2)
+print("1 + 2 =", 1 + 2)
 print("원하는 문자열을 출력할 때는 print함수를 이용함")
+
+num1 = int(input("첫 번째 숫자를 입력하세요: "))
+num2 = int(input("두 번째 숫자를 입력하세요: "))
+
+print(f"{num1} + {num2} = {num1 + num2}")
